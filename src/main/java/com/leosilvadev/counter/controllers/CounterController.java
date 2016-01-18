1d0c41e536577f61df5b23b5c5013714fdb4ccd3
Leonardo JG Silva <leosilvadev@gmail.com>
2016-01-17 22:02:08 -0200

    adding retrolambda

--- src/main/java/com/leosilvadev/counter/controllers/CounterController.java
+++ src/main/java/com/leosilvadev/counter/controllers/CounterController.java
@@ -5,10 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
 
 @RestController
@@ -22,10 +19,8 @@ public class CounterController {
 	public void sumOf(@PathVariable Double valueOne, @PathVariable Double valueTwo){
 		JsonObject json = new JsonObject().put("valueOne", valueOne).put("valueTwo", valueTwo);
 		
-		vertx.eventBus().send("vertx.counter.sum", json, new Handler<AsyncResult<Message<Double>>>() {
-			public void handle(AsyncResult<Message<Double>> message) {
-				System.out.println(message.result().body());
-			}
+		vertx.eventBus().send("vertx.counter.sum", json, (message) -> {
+			System.out.println(message.result().body());
 		});
 	}
 
